f1105b12b5ec8ff05306a1487408034cc4eeebae
Suzane Ringoringo <suzaneringoring@gmail.com>
2018-03-24 12:57:53 +0700

    Rafid's

--- main.cpp
+++ main.cpp
@@ -8,21 +8,30 @@ void display() {  // Display function will draw the image.
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glBegin(GL_TRIANGLES);
-	glColor3f(1, 1, 1); // white
-	glVertex2f(-1, -1);
-	glVertex2f(1, -1);
-	glVertex2f(0, 1);
+	glColor3f(0, 0, 1); // Blue
+	glVertex2f(-1, -0.5);
+	glColor3f(0, 1, 0); // Green
+	glVertex2f(0, -0.5);
+	glColor3f(1, 0, 0); // Red
+	glVertex2f(-0.5, 0.5);
+	glEnd();
+
+	glBegin(GL_TRIANGLES);
+	glColor3f(1, 0, 0); // Blue
+	glVertex2f(0, -0.5);
+	glVertex2f(1, -0.5);
+	glVertex2f(0.5, 0.5);
 	glEnd();
 
 	glutSwapBuffers(); // Required to copy color buffer onto the screen.
 
-}
+} 
 
 int main(int argc, char** argv) {  // Initialize GLUT and 
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE);    // Use single color buffer and no depth buffer.
-	glutInitWindowSize(500, 500);         // Size of display area, in pixels.
+	glutInitWindowSize(800, 800);         // Size of display area, in pixels.
 	glutInitWindowPosition(100, 100);     // Location of window in screen coordinates.
 	glutCreateWindow("GL RGB Triangle"); // Parameter is window title.
 	glutDisplayFunc(display);            // Called when the window needs to be redrawn.
